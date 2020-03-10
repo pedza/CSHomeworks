51452e0af5994db531b648d87b393f460ae8f3c9
pedza <predrag-sk@hotmail.com>
2020-03-10 21:12:49 +0100

    Add project files.

---
+++ Homework1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a number between 1- 3");
+           int number= Convert.ToInt32(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("You Got a new Car");
+                    break;
+                case 2:
+                    Console.WriteLine("You got a new plane");
+                    break;
+                case 3:
+                    Console.WriteLine("you got a new bike");
+                    break;
+                default:
+                    Console.WriteLine("Not a valid number");
+                    break;
+            }
+        }
+    }
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine($"{num1} is the biggest num");
+                if (num1 % 2 == 0)
+                {
+                    Console.WriteLine($"{num1} is even number");
+                }
+                else
+                {
+                    Console.WriteLine($"{num1} is odd number");
+                }
+            }
+
+            else
+            {
+            
+            Console.WriteLine($"{num2} is the biggest num");
+
+            if (num2 % 2 == 0)
+            {
+                Console.WriteLine($"{num2} is even number");
+            }
+            else
+            {
+                Console.WriteLine($"{num2} is odd number");
+            }
+
+            }
+
+        }
+    }
+}
