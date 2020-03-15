09951be83fc94340d561f01518b95cacff285cc1
pedza <predrag-sk@hotmail.com>
2020-03-15 21:45:59 +0100

    Homework SumOfEven and StudentGroup

---
+++ Class 3 Task 1 Homework/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Class_3_Task_1_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] arrayInt = new int[6];
+            int sum = 0;
+            for (int i = 0; i<6; i++)
+            {
+                Console.WriteLine($"enter humber {i+1}");
+               arrayInt[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (arrayInt[i] % 2 == 0)
+                {
+                    sum += arrayInt[i];
+                }
+
+            };
+            Console.WriteLine($"the sum of the even numbers is {sum}");
+            Console.Read();
+
+        }
+    }
+}
---
+++ StudentGroup/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = {"Pedza", "Ivica", "Dejan", "Vesna" };
+            string[] studentsG2= { "Krsto", "Zlatko", "Petko" };
+            Console.WriteLine("Enter a number of the Group (between 1 and 2)");
+            int groupInput = Convert.ToInt32(Console.ReadLine());
+
+            if (groupInput == 1)
+            {
+                Console.WriteLine("The students from G1 are:");
+                for(int i =0; i<studentsG1.Length; i++)
+                {
+                    Console.WriteLine(studentsG1[i]);
+                }
+
+            }
+            else if (groupInput == 2)
+            {
+                Console.WriteLine("The students from G2 are:");
+                for (int i = 0; i < studentsG2.Length; i++)
+                {
+                    Console.WriteLine(studentsG2[i]);
+                }
+            }
+            else { Console.WriteLine("The group doesnt exist"); }
+
+        }
+    }
+}
